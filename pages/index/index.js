c14f7533720f1e16436851ce0d48d3bc372f38b6
Myron Zhang <myron.z.zhang@gmail.com>
2020-02-11 23:26:29 +0800

    add dynamic weather information

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,1 +1,61 @@
-Page({})
+const weatherMap = {
+  'sunny': 'sunny',
+  'cloudy': 'cloudy',
+  'overcast': 'overcast',
+  'lightrain': 'light rain',
+  'heavyrain': 'heavy rain',
+  'snow': 'snow'
+}
+
+const weatherColorMap = {
+  'sunny': '#cbeefd',
+  'cloudy': '#deeef6',
+  'overcast': '#c6ced2',
+  'lightrain': '#bdd5e1',
+  'heavyrain': '#c5ccd0',
+  'snow': '#aae1fc'
+}
+
+Page({
+  data: {
+    nowTemp: '14°',
+    nowWeather: 'cloudy',
+    nowWeatherBackground: ''
+  },
+  
+  onPullDownRefresh() {
+    this.getNow(() => {
+      wx.stopPullDownRefresh()
+    });
+  },
+
+  onLoad() {
+    this.getNow();
+  },
+  
+  getNow(callback) {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now', data: {
+        city: 'haikou'
+      },
+      success: res => {
+        let result = res.data.result
+        let temp = result.now.temp
+        let weather = result.now.weather
+        console.log(result)
+        this.setData({
+          nowTemp: temp + '°',
+          nowWeather: weatherMap[weather],
+          nowWeatherBackground: '/images/' + weather + '-bg.png'
+        })
+        wx.setNavigationBarColor({
+          frontColor: '#000000',
+          backgroundColor: weatherColorMap[weather],
+        })
+      },
+      complete: () => {
+        callback && callback()
+      }
+    })
+  }
+})
